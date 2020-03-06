02c4712aacea79a809c602d22dafebcbdf199f73
Jon <61868371+jonsoln@users.noreply.github.com>
2020-03-06 13:22:16 +0100

    Created launcher file
    Launcher file in psuedocode-ish. Will continue to edit once the class file is created

---
+++ Launcher_Jon.py
@@ -0,0 +1,26 @@
+import ModelClass as MC
+
+stpFilePath = 'C:\Users\Jon\OneDrive\FleksProp\HW_2020_02\HydroWing.stp'
+loadTxtFilePath = 'C:\Users\Jon\OneDrive\FleksProp\Scripts\load.txt'
+inputFileLocation = 'C:\Users\Jon\Documents'
+
+propellerType = 'HW' # or propeller = 'AZP'
+plottedRadii = [0.5,0.6,0.7,0.8,0.9]
+partition = 'Fan' # or partition = 'Grid', partition = 'Horizontal', partition = 'Vertical'
+partitionRefinement = 5
+shellOrSolid = 'Solid' # or shellOrSolid = 'Solid'
+cellOrFace = 'Cell' # or cellOrFace = 'Face'
+side = 'P' # or side = 'S'
+
+
+propeller = MC(propellerType,plottedRadii,partition,partitionRefinement,shellOrSolid,cellOrFace,side,inputFileLocation)
+
+propeller.prep()
+propeller.partition()
+propeller.createInput()
+
+
+
+
+
+

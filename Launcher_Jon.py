ebcd944e254b7870b5a23a04a13c94f4b7f9e448
Jon <61868371+jonsoln@users.noreply.github.com>
2020-03-09 13:48:09 +0100

    Launcher file for HWAllOver
    Updated launcher file
    
    Deleted some variables not in use

--- Launcher_Jon.py
+++ Launcher_Jon.py
@@ -3,11 +3,6 @@ import numpy as np
 caeFilePath = 'C:\Users\Jon\OneDrive\FleksProp\Scripts\HW.cae'
 inputFileLocation = 'C:\Users\Jon\OneDrive\FleksProp\InputFiles'
 
-partition = 'AllOver' # or partition = 'Fan', partition = 'Grid', partition = 'Horizontal', partition = 'Vertical', partition = AllOver
-#partitionRefinement = 5
-shellOrSolid = 'Solid' # or shellOrSolid = 'Solid'
-#cellOrFace = 'Cell' # or cellOrFace = 'Face'
-#side = 'P' # or side = 'S', side = 'PS'
 plyAngleLimits = [-90,90] # or nothing
 plyAngleNumber = 3 #
 plyThickness = 0.2

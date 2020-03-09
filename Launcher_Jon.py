01acc532d7ace6644ced072f16cbdef851d68f72
Jon <61868371+jonsoln@users.noreply.github.com>
2020-03-09 12:09:14 +0100

    Fixed errors - The Launcher and Class files are able to create a full laminate
    Fixed errors - The Launcher and Class files are able to create a full laminate

--- Launcher_Jon.py
+++ Launcher_Jon.py
@@ -1,4 +1,4 @@
-import HydroWing
+from HydroWing import HydroWing
 import numpy as np
 caeFilePath = 'C:\Users\Jon\OneDrive\FleksProp\Scripts\HW.cae'
 inputFileLocation = 'C:\Users\Jon\OneDrive\FleksProp\InputFiles'
@@ -8,16 +8,14 @@ partition = 'AllOver' # or partition = 'Fan', partition = 'Grid', partition = 'H
 shellOrSolid = 'Solid' # or shellOrSolid = 'Solid'
 #cellOrFace = 'Cell' # or cellOrFace = 'Face'
 #side = 'P' # or side = 'S', side = 'PS'
-plyAngleLimits = [-90,90], # or nothing
-plyAngleStep = 5 #
+plyAngleLimits = [-90,90] # or nothing
+plyAngleNumber = 5 #
 plyThickness = 0.2
 
 propeller = HydroWing(caeFilePath)
-
-propeller.AllOver(np.linspace(plyAngleLimits,plyAngleStep), plyThickness, inputFileLocation)
-
-
-
+print(plyAngleLimits[0])
+print(plyAngleLimits[1])
+propeller.all_over(np.linspace(plyAngleLimits[0],plyAngleLimits[1],3), plyThickness, inputFileLocation)
 
 
 

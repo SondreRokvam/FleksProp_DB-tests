472e221351388f75074146d8a5986a53de3cbc93
SondreRokvam <36338470+SondreRokvam@users.noreply.github.com>
2020-03-25 16:21:57 +0100

    SammenlikneDesign

--- Lag_batchFiler_for_inpFiler.py
+++ Lag_batchFiler_for_inpFiler.py
@@ -5,14 +5,21 @@ Created on Wed Mar 11 16:39:31 2020
 """
 import os
 
-os.chdir('C:\\Users\\Sondre\\Desktop\\HW\\')
+os.chdir('C:\\Users\\Sondre\\Desktop\\azp\\')
 
 current=os.getcwd()
 print(current+'\\')
 # Hent
-for g in os.listdir(current):
+Batpaths =[]
+for g in [f for f in os.listdir(current) if not f.endswith('.bat')]:
     odb_names = [f for f in os.listdir(current+'\\'+g+'\\') if (f.endswith('.inp')) ]
     s=open(current+'\\'+g+'\\'+'run_inputs.bat',"w+")
+    Batpaths.append(current+'\\'+g+'\\'+'run_inputs.bat')
     for od in odb_names:
         s.write('call abq2017 job='+od[:-4]+ ' int\n')
     s.close()
+"""
+p=open(current+'\\batch.bat',"w+")
+for bat in Batpaths:
+    p.write('call '+bat+'\n')
+p.close()"""
